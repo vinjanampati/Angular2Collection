420dca8f8d79ca5bdf74f4e3bd5c2d2fb4ae0fbd
DenDal <DenDal@DenDal.com>
2017-02-19 13:25:57 -0600

    Commiting the project.

---
+++ Angular2HelloWorld/Angular2HelloWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Angular2HelloWorld.Startup))]
+namespace Angular2HelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
